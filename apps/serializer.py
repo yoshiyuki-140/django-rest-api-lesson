6bb26644de099148f533d1c1a2366d78abf659ef
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-03 15:37:49 +0900

    clean : serializerに一行コメントを追加

--- apps/serializer.py
+++ apps/serializer.py
@@ -7,4 +7,6 @@ from apps.models import Object
 class ObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Object
+
+        # モデルに定義されているフィールドを全て指定
         fields = '__all__'
