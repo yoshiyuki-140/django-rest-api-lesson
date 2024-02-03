acaf5621987bd2f68a56cd8e462f7a9f373efc46
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-03 15:05:38 +0900

    add : テスト用のObjectをmodels.pyに追加

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Object(models.Model):
+    """restframeworkの実験用オブジェクト
+    """
+    name = models.CharField(max_length=100)
+    text = models.TextField()
