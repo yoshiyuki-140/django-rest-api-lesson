82787efcca4013040311b2c678530ac662703cb3
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-03 15:36:32 +0900

    add : ViewSetsの追加

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, filters
 
-# Create your views here.
+
+from apps.models import Object
+from apps.serializer import ObjectSerializer
+
+
+class ObjectViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Object class
+    """
+    queryset = Object.objects.all()
+    serializer_class = ObjectSerializer
